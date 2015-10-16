0344ae06b00fc7206b9d3588be76bf13f206ece4
liaokn@huizuche.com <liaokn@huizuche.com>
2015-10-16 18:48:56 +0800

    merge conflict

--- api/src/main/java/iqq/api/event/IMEvent.java
+++ api/src/main/java/iqq/api/event/IMEvent.java
@@ -1,19 +1,19 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
  * Project  : IQQ_V2
@@ -24,7 +24,6 @@
  * License  : Apache License 2.0 
  */
 package iqq.api.event;
-
 
 import iqq.api.common.EventBase;
 
@@ -37,35 +36,32 @@ import java.io.Serializable;
  * @author solosky <solosky772@qq.com>
  *
  */
-public class IMEvent extends EventBase implements Serializable{
-    private static final long serialVersionUID = 1L;
-    private IMEventType type;
+public class IMEvent extends EventBase implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private IMEventType type;
 
-    public IMEvent(IMEventType type) {
-        this.type = type;
-    }
+	public IMEvent(IMEventType type) {
+		this.type = type;
+	}
 
-    public IMEvent() {
-    }
+	public IMEvent() {
+	}
 
-    public IMEvent(IMEventType type, Object data) {
-        this.type = type;
-        this.setTarget(data);
-    }
+	public IMEvent(IMEventType type, Object data) {
+		this.type = type;
+		this.setTarget(data);
+	}
 
+	public IMEventType getType() {
+		return type;
+	}
 
-    public IMEventType getType() {
-        return type;
-    }
+	public void setType(IMEventType type) {
+		this.type = type;
+	}
 
-    public void setType(IMEventType type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return "IMEvent{" +
-                "type=" + type +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "IMEvent{" + "type=" + type + '}';
+	}
 }
